72d433bfb362d079c8d38c95069e1e2518bb9dc1
Franperman23 <franperman2323@gmail.com>
2026-02-06 12:38:16 +0100

    feat: añadir repositorio para la entidad Ingrediente

---
+++ Pasteleria/Pasteleria/src/main/java/com/example/demo/repository/IngredienteRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.Ingrediente;
+
+public interface IngredienteRepository extends JpaRepository<Ingrediente, Long> {
+}
