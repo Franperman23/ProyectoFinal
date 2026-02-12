a345da53fd9587b997401ad5f151acf2c0a01d97
Franperman23 <franperman2323@gmail.com>
2026-02-12 18:15:28 +0100

    Añadir las dos clases para el control de errores

---
+++ Pasteleria/Pasteleria/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        Map<String, Object> details = new HashMap<>();
+        details.put("timestamp", new Date());
+        details.put("estado", "ERROR");
+        details.put("mensaje", ex.getMessage());
+        details.put("detalles", request.getDescription(false));
+        return new ResponseEntity<>(details, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> globalExceptionHandler(Exception ex, WebRequest request) {
+        Map<String, Object> details = new HashMap<>();
+        details.put("timestamp", new Date());
+        details.put("mensaje", "Error interno en el servidor de la Pastelería");
+        return new ResponseEntity<>(details, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
