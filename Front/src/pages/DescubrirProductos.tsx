ab7d3a4467bcff7bb15fe3c5a704c12b374f1122
Ruben Maestro <rubenmaestroromero@gmail.com>
2026-02-13 09:33:15 +0100

    feat: primer fetch hecho de productos

--- Front/src/pages/DescubrirProductos.tsx
+++ Front/src/pages/DescubrirProductos.tsx
@@ -3,9 +3,29 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import { CartContext } from "../context/CartContext";
+import type { ListarProductoDTO } from "../types/ListarProductoDTO";
 
 const DescubrirProductos: React.FC = () => {
+  const [listaProductos, setListaProductos] = React.useState<ListarProductoDTO[]>([]);
+
   const { addToCart } = useContext(CartContext);
+
+  async function fetchProductos() {
+    try {
+      const response = await fetch("/api/productos");
+      if (!response.ok) {
+        throw new Error("Error al cargar los productos");
+      }
+      const data: ListarProductoDTO[] = await response.json();
+      setListaProductos(data);
+    } catch (error) {
+      console.error("Error al cargar los productos:", error);
+    }
+  }
+
+  React.useEffect(() => {
+    fetchProductos();
+  }, []);
 
   return (
     <>
@@ -29,6 +49,23 @@ const DescubrirProductos: React.FC = () => {
           <div className="grid fade-up">
 
             {/* ---------------- PANADERÍA (8) ---------------- */}
+
+            {listaProductos.map((producto) => (
+           
+            <article className="card" key={producto.id}>
+              <div className="card-image">
+                <img src={producto.imagen} alt={producto.nombre} loading="lazy" />
+              </div>
+              <div className="card-body">
+                <h3>{producto.nombre}</h3>
+                <p>{producto.descripcion}</p>
+                <p className="price">{producto.precio.toFixed(2)} €</p>
+                <button className="btn add-cart"
+                  onClick={() => addToCart(producto)}
+                >Añadir al carrito</button>
+              </div>
+            </article>
+            ))}
 
             {/* 1 */}
             <article className="card">
